6d526d3ea1134f2d3b8f288cbe124190c3ed8c46
Martin Flaherty <flaherty.martin@gmail.com>
2022-04-19 15:18:33 -0400

    Module 17.03

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -19,5 +19,13 @@ namespace ElevenNote.WebAPI.Controllers
         {
             _noteService = noteService;
         }
+
+        // GET api/Note
+        [HttpGet]
+        public async Task<IActionResult> GetAllNotes()
+        {
+            var notes = await _noteService.GetAllNotesAsync();
+            return Ok(notes);
+        }
     }
 }
